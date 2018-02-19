ea0d0fd8b6d81e04f237dbd74712ddc6d93b42c4
Elizabeth Eckels <elizabeth.r.eckels@gmail.com>
2018-02-19 23:29:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,11 @@
 puts "Welcome to Tic Tac Toe"
 
 def display_board
-puts
-  "   |   |   ","-----------","   |   |   ","-----------","   |   |   "
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
 end
 
 display_board
