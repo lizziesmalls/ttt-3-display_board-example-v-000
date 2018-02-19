5f3218c712ee6405b59cdd2f7559eb29336df5f5
Elizabeth Eckels <elizabeth.r.eckels@gmail.com>
2018-02-19 23:16:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-puts "Welcome to Tic Tac Toe"
-puts "   |   |   ","-----------","   |   |   ","-----------","   |   |   "
-
+puts "Welcome to Tic Tac Toe",
+  "   |   |   ",
+  "-----------",
+  "   |   |   ",
+  "-----------",
+  "   |   |   ",
+  "What move would X like to make? (1-9)"
+  
